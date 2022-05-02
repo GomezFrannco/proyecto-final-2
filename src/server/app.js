e8c25021c1552b30f61555da598f8ace76e7b8e9
Fran <los321gatoslocos@gmail.com>
2022-05-02 15:59:46 -0300

    [Fix] fixed a property in server class

--- src/server/app.js
+++ src/server/app.js
@@ -3,32 +3,33 @@ import productRouter from "../routes/products.routes.js";
 import cartRouter from "../routes/carts.routes.js";
 
 export class Server {
-  constructor() {
-    this.app = {
-      express: express(),
-      port: null,
-    };
+  constructor(port) {
+    this.port = port;
+    this.app = express();
     this.middlewares();
     this.appRoutes();
     this.routerRoutes();
   }
+  settings() {
+    this.app.set("port", process.env.PORT || this.port || 8080);
+  }
   middlewares() {
-    this.app.express.use(express.json());
+    this.app.use(express.json());
   }
   appRoutes() {
-    this.app.express.get("/", (req, res) => {
+    this.app.get("/", (req, res) => {
       res.send("Hi from express");
     });
   }
   routerRoutes() {
-    this.app.express.use("/api", productRouter);
-    this.app.express.use("/api", cartRouter);
+    this.app.use("/api", productRouter);
+    this.app.use("/api", cartRouter);
   }
   listen(port) {
-    this.app.express.listen((this.app.port = port), () => {
-      console.log("listening on port:", (this.app.port = port));
+    this.app.listen(this.app.get("port"), () => {
+      console.log("listening on port: ", this.app.get("port"));
     });
-    this.app.express.on("error", (err) => {
+    this.app.on("error", (err) => {
       console.log("error: ", err);
     });
   }
